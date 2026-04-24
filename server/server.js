e3ab2e6ae90956c69aba9f44cb74aca183416d4f
Aarsh Singh <starfire9599@gmail.com>
2026-04-24 23:57:09 +0530

    Main file API routing

--- server/server.js
+++ server/server.js
@@ -2,7 +2,16 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import multer from "multer";
+
 import connectDB from "./config/db.js";
+
+import authRouter from "./routes/authRoutes.js";
+import employeeRouter from "./routes/employeeRoutes.js";
+import profileRouter from "./routes/profileRoutes.js";
+import attendanceRouter from "./routes/attendanceRoutes.js";
+import leaveRouter from "./routes/leaveRoutes.js";
+import payslipRouter from "./routes/payslipsRoutes.js";
+
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -14,8 +23,14 @@ app.use(multer().none());
 
 // Routes
 app.get("/", (req, res) => res.send("Server is running..."));
+app.use("/api/auth", authRouter);
+app.use("/api/employees", employeeRouter);
+app.use("/api/profile", profileRouter);
+app.use("/api/attendance", attendanceRouter);
+app.use("/api/leave", leaveRouter);
+app.use("/api/payslips", payslipRouter);
 
-await connectDB()
+await connectDB();
 app.listen(PORT, () => {
   console.log(`Sevrer is running on PORT: ${PORT}`);
 });
