9c486c1323fc6ebd6fa52d987b242a01c8ae6e57
Aarsh Singh <starfire9599@gmail.com>
2026-04-25 10:49:28 +0530

    feat: implement Inngest event handler for auto clock-out and reminder emails after 9 hours

---
+++ server/inngest/index.js
@@ -0,0 +1,73 @@
+import { Inngest } from "inngest";
+import Attendance from "../models/Attendance.js";
+import Employee from "../models/Employee.js";
+import LeaveApplication from "../models/LeaveApplication.js";
+import sendEmail from "../config/nodemailer.js";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "ems" });
+
+// Auto Check-out for employees
+const autoCheckOut = inngest.createFunction(
+  { id: "auto-check-out", triggers: [{ event: "employee/check-out" }] },
+  async ({ event, step }) => {
+    const { employeeId, attendanceId } = event.data;
+
+    // Wait for 9 hours
+    await step.sleepUntil(
+      "wait-for-the-9-hours",
+      new Date(new Date().getTime() + 9 * 60 * 60 * 1000),
+    );
+
+    // Get attendance data
+    let attendance = await Attendance.findById(attendanceId);
+
+    if (!attendance?.checkOut) {
+      // Get employee data
+      const employee = await Employee.findById(employeeId);
+
+      // Send reminder email
+      await sendEmail({
+        to: employee.email,
+        subject: "Attendance Check-Out Remainder",
+        body: `
+                <div style="max-width: 600px;">
+                    <h2>Hi ${employee.firstName}, 👋</h2>
+                    <p style="font-size: 16px;">You have a check-in in ${employee.department} today:</p>
+                    <p style="font-size: 18px; font-weight: bold; color: #007bff; margin: 8px 0;">${attendance?.checkIn?.toLocaleTimeString()}</p>
+                    <p style="font-size: 16px;">Please make sure to check-out in one hour.</p>
+                    <p style="font-size: 16px;">If you have any questions, please contact your admin.</p>
+                    <br />
+                    <p style="font-size: 16px;">Best Regards,</p>
+                    <p style="font-size: 16px;">EMS</p>
+                </div>
+            `,
+      });
+
+      // After 10 hours, marked attendance as checked out with status "LATE"
+      await step.sleepUntil(
+        "wait-for-the-1-hour",
+        new Date(new Date().getTime() + 1 * 60 * 60 * 1000),
+      );
+
+      attendance = await Attendance.findById(attendanceId);
+      if (!attendance?.checkOut) {
+        attendance.checkOut =
+          new Date(attendance.checkIn).getTime() + 4 * 60 * 60 * 1000;
+        attendance.workingHours = 4;
+        attendance.dayType = "Half Day";
+        attendance.status = "LATE";
+        await attendance.save();
+      }
+    }
+  },
+);
+
+
+
+
+
+// Create an empty array where we'll export future Inngest functions
+export const functions = [
+  autoCheckOut,
+];
